ee365ee2b0a0c72e7665ed59a34e3f20b07f1e9a
Shreyansh-Rai <shreyanshrajeevarai@gmail.com>
2021-02-22 13:27:56 +0530

    update1

--- Zankyo1.py
+++ Zankyo1.py
@@ -1,13 +1,22 @@
 from tkinter import *
+from tkinter import filedialog
 import pygame
 
 loop=Tk()
+def addsong():
+    addedsong= filedialog.askopenfilename(initialdir="\Audio",title="Pick Song",filetypes=(("mp3 Files","*.mp3"),))
+    l=addedsong.split('/')
+    s=l[len(l)-1]
+    l=s.split(".")
+    playground.insert(END,str(l[0]))
+
+
 loop.title("Zankyo Player")
 loop.iconbitmap("Graphics\\ZankyoIcon2.ico")
 loop.geometry("500x400")
-loop.configure(bg="#006666")
-backg=Listbox(loop,bg="#669999",fg="#00cc99",width=80,bd=5)
-backg.pack(pady=10)
+loop.configure(bg="#ffb366")
+playground=Listbox(loop,bg="#ffcc99",fg="#000000",width=80,bd=5,selectbackground="#ff944d",selectforeground="#000000")
+playground.pack(pady=10)
 pygame.mixer.init()
 playimg=PhotoImage(file="Graphics\\Playb.png")
 pauseimg=PhotoImage(file="Graphics\\Pauseb.png")
@@ -16,14 +25,22 @@ previmg=PhotoImage(file="Graphics\\Prevb.png")
 stopimg=PhotoImage(file="Graphics\\Stopb.png")
 imagespace=Frame(loop)
 imagespace.pack()
-playbt=Button(imagespace,image=playimg,borderwidth=0,bg="#006666")
-pausebt=Button(imagespace,image=pauseimg,borderwidth=0,bg="#006666")
-prevbt=Button(imagespace,image=previmg,borderwidth=0,bg="#006666")
-nextbt=Button(imagespace,image=nextimg,borderwidth=0,bg="#006666")
-stopbt=Button(imagespace,image=stopimg,borderwidth=0,bg="#006666")
+playbt=Button(imagespace,image=playimg,borderwidth=0,bg="#ffb366")
+pausebt=Button(imagespace,image=pauseimg,borderwidth=0,bg="#ffb366")
+prevbt=Button(imagespace,image=previmg,borderwidth=0,bg="#ffb366")
+nextbt=Button(imagespace,image=nextimg,borderwidth=0,bg="#ffb366")
+stopbt=Button(imagespace,image=stopimg,borderwidth=0,bg="#ffb366")
 prevbt.grid(row=0,column=0)
 playbt.grid(row=0,column=1)
 pausebt.grid(row=0,column=2)
 stopbt.grid(row=0,column=3)
 nextbt.grid(row=0,column=4)
+
+menu1=Menu(loop)
+loop.config(menu=menu1) # sort of like main menu widget
+#For adding songs
+dropaddmenu=Menu(menu1) #a menu we need 
+menu1.add_cascade(label="+Songs",menu=dropaddmenu) #add a cascade menu to the main widget and call it dropadd menu
+dropaddmenu.config(bg="#d2a679")
+dropaddmenu.add_command(label="Add A Song To The Playlist",command=addsong)
 loop.mainloop()
