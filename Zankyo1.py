260453e9173de0681f2048d1fff2c220007b517c
Genkai <75561611+Shreyansh-Rai@users.noreply.github.com>
2021-03-15 18:21:49 +0530

    Change To GUI Play button Fix Partial slider implemented
    Slider will be worked on later as well

--- Zankyo1.py
+++ Zankyo1.py
@@ -1,3 +1,4 @@
+
 from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
@@ -13,6 +14,7 @@ import urllib
 import base64
 from io import BytesIO
 from PIL import ImageTk, Image
+from mutagen.mp3 import MP3
 
 CLIENT_ID = 'eab56b94c3e8449794b46717080609b9'
 CLIENT_SECRET = 'bd23f78d31fb4c7bbdda106db8484151'
@@ -112,36 +114,62 @@ def onselect(evt):
     e2.delete(0,END)
     e2.insert(END,value)
 
-
+songlen=0
+currpos=0
 
 def addsongs():
-    addedsongs= filedialog.askopenfilenames(initialdir="\Audio",title="Pick Song",filetypes=(("mp3 Files","*.mp3"),))
+    addedsongs= filedialog.askopenfilenames(initialdir="/Audio",title="Pick Song",filetypes=(("mp3 Files","*.mp3"),))
     for song in addedsongs:
         l=((song.split('/'))[-1]).split('.')
         playground.insert(END,str(l[0]))
 
+shiftit=0
 def playsong():
-    song = playground.get(ACTIVE)
-    playground.selection_set(ACTIVE, last = None)
-    e2.delete(0,END)
-    e2.insert(END,song)
-
-    song = '/Users/pramodkhandelwal/github/PythonZankyo/Audio/' + song + '.mp3'
-    pygame.mixer.music.load(song)
-    pygame.mixer.music.play(loops = 0)
+    global pause_state 
+    global songlen
+    global currpos
+    global shiftit
+    if(pause_state):  
+        pygame.mixer.music.unpause()
+        pause_state=False
+    else:  
+        song = playground.get(ACTIVE)
+        song = f'C:\\Users\\Shreyansh\\Desktop\\SnekPirojekt\\PythonZankyo\\Audio\\{song}.mp3'
+        pygame.mixer.music.load(song)
+        thissong=MP3(song)
+        songlen=int(thissong.info.length)
+        m1,s1=divmod(songlen,60)
+        if s1>=10:
+            lentime.config(text=f'{m1}:{s1}')
+        else:
+            lentime.config(text=f'{m1}:0{s1}')
+        def timemysong():
+            
+            currpos=int(pygame.mixer.music.get_pos()/1000)
+            shiftit=currpos/songlen*100
+            timescrub.configure(value=shiftit)
+            m2,s2=divmod(currpos,60)
+            
+            postime.config(text=f'{m2}:{s2}')
+            if(currpos==songlen):
+                stopsong()
+            postime.after(1000,timemysong)
+        
+        timemysong()
+        pygame.mixer.music.play(loops = 0)
 
 def stopsong():
     pygame.mixer.music.stop()
     playground.selection_clear(ACTIVE)
     e2.delete(0,END)
     playground.activate(0)
-    imagespace3.pack_forget()
+    
 
+pause_state = False
 
-
-def pausesong():
+def pausesong(is_paused):
     global pause_state
-
+    pause_state = is_paused
     if pause_state:
         pygame.mixer.music.unpause()
         pause_state = False
@@ -302,7 +330,7 @@ def Clearplaylist():
     playground.delete(0,END)
     pygame.mixer.music.stop()
 
-def Themesetter(loop,playground,playbt,pausebt,nextbt,prevbt,stopbt,colscheme):
+def Themesetter(scrubFrame,shufbt,imagespace,imagespace1,imagespace2,loop,masterFrame,volumeFrame,playground,playbt,pausebt,nextbt,prevbt,stopbt,postime,lentime,colscheme):
     if colscheme=="MONOCHROME":
         loop.configure(bg="#454545")
         playground.config(bg="#999999",fg="#ffffff",selectbackground="#707070",selectforeground="#ffffff")
@@ -311,6 +339,16 @@ def Themesetter(loop,playground,playbt,pausebt,nextbt,prevbt,stopbt,colscheme):
         prevbt.config(bg="#999999")
         nextbt.config(bg="#999999")
         stopbt.config(bg="#999999")
+        postime.config(bg="#454545",fg="#ffffff")
+        lentime.config(bg="#454545",fg="#ffffff")
+        masterFrame.configure(bg="#454545")
+        volumeFrame.configure(bg="#454545")
+        imagespace.configure(bg="#454545")
+        imagespace1.configure(bg="#454545")
+        imagespace2.configure(bg="#454545")
+        shufbt.configure(bg="#999999")
+        scrubFrame.configure(bg="#454545")
+
 
     if colscheme=="Glacier":
         loop.configure(bg="#20c3d0")
@@ -320,6 +358,15 @@ def Themesetter(loop,playground,playbt,pausebt,nextbt,prevbt,stopbt,colscheme):
         prevbt.config(bg="#20c3d0")
         nextbt.config(bg="#20c3d0")
         stopbt.config(bg="#20c3d0")
+        postime.config(bg="#20c3d0")
+        lentime.config(bg="#20c3d0")
+        masterFrame.configure(bg="#20c3d0")
+        volumeFrame.configure(bg="#20c3d0")
+        imagespace.configure(bg="#20c3d0")
+        imagespace1.configure(bg="#20c3d0")
+        imagespace2.configure(bg="#20c3d0")
+        shufbt.config(bg="#20c3d0")
+        scrubFrame.configure(bg="#20c3d0")
 
     if colscheme=="Autumn Leaves" :
         loop.configure(bg="#98482b")
@@ -329,6 +376,15 @@ def Themesetter(loop,playground,playbt,pausebt,nextbt,prevbt,stopbt,colscheme):
         prevbt.config(bg="#98482b")
         nextbt.config(bg="#98482b")
         stopbt.config(bg="#98482b")
+        postime.config(bg="#98482b",fg="#ffffff")
+        lentime.config(bg="#98482b",fg="#ffffff")
+        masterFrame.configure(bg="#98482b")
+        volumeFrame.configure(bg="#98482b")
+        imagespace.configure(bg="#98482b")
+        imagespace1.configure(bg="#98482b")
+        imagespace2.configure(bg="#98482b")
+        shufbt.config(bg="#98482b")
+        scrubFrame.configure(bg="#98482b")
 
     if colscheme=="Spring" :
         loop.configure(bg="#54862e")
@@ -338,6 +394,15 @@ def Themesetter(loop,playground,playbt,pausebt,nextbt,prevbt,stopbt,colscheme):
         prevbt.config(bg="#54862e")
         nextbt.config(bg="#54862e")
         stopbt.config(bg="#54862e")
+        postime.config(bg="#54862e",fg="#ffffff")
+        lentime.config(bg="#54862e",fg="#ffffff")
+        masterFrame.configure(bg="#54862e")
+        volumeFrame.configure(bg="#54862e")
+        imagespace.configure(bg="#54862e")
+        imagespace1.configure(bg="#54862e")
+        imagespace2.configure(bg="#54862e")
+        shufbt.config(bg="#54862e")
+        scrubFrame.configure(bg="#54862e")
 
     if colscheme=="Summer" :
         loop.configure(bg="#F3872F")
@@ -346,32 +411,65 @@ def Themesetter(loop,playground,playbt,pausebt,nextbt,prevbt,stopbt,colscheme):
         pausebt.config(bg="#F3872F")
         prevbt.config(bg="#F3872F")
         nextbt.config(bg="#F3872F")
-        stopbt.config(bg="#")
+        stopbt.config(bg="#F3872F")
+        postime.config(bg="#F3872F")
+        lentime.config(bg="#F3872F")
+        masterFrame.configure(bg="#F3872F")
+        volumeFrame.configure(bg="#F3872F")
+        imagespace.configure(bg="#F3872F")
+        imagespace1.configure(bg="#F3872F")
+        imagespace2.configure(bg="#F3872F")
+        shufbt.config(bg="#F3872F")
+        scrubFrame.configure(bg="#F3872F")
 
 
 
+def volume(x):
+  pygame.mixer.music.set_volume(1 - sliderVolume.get())
+  currentVolume = pygame.mixer.music.get_volume()
+  #sliderText.config(text = int(currentVolume * 100))
+
+
+def scrub(x):
+    global shiftit
+    toset=timescrub.get()*songlen/100
+    shiftit=toset*100
+    timescrub.configure(value=shiftit)
+    pygame.mixer.music.set_pos(toset)
+    pass
+    
 
 
 
 
 loop.title("Zankyo Player")
-loop.iconbitmap("Graphics/ZankyoIcon2.ico")
-loop.geometry("500x400")
+# loop.iconbitmap("Graphics/ZankyoIcon2.ico")
+loop.geometry("900x480")
 loop.configure(bg="#F3872F")
-playground=Listbox(loop,bg='#ffcc99',fg="#000000",width=80,bd=5,selectbackground="#ff944d",selectforeground="#000000")
-playground.bind('<<ListboxSelect>>', onselect)
-playground.pack(pady=10)
-pygame.mixer.init()
-playimg=PhotoImage(file="Graphics/Playb.png")
-pauseimg=PhotoImage(file="Graphics/Pauseb.png")
-nextimg=PhotoImage(file="Graphics/Nextb.png")
-previmg=PhotoImage(file="Graphics/Prevb.png")
-stopimg=PhotoImage(file="Graphics/Stopb.png")
 
-imagespace=Frame(loop,height = 2)
-imagespace.pack()
+masterFrame = Frame(loop)
+masterFrame.configure(bg="#F3872F")
+masterFrame.pack(pady = 20)
+
+playground=Listbox(masterFrame,bg='#ffcc99',fg="#000000",width=80,bd=5,selectbackground="#ff944d",selectforeground="#000000")
+playground.bind('<<ListboxSelect>>', onselect)
+playground.grid(row = 0, column = 0)
+pygame.mixer.init()
+playimg=PhotoImage(file="Graphics\\Playb.png")
+pauseimg=PhotoImage(file="Graphics\\Pauseb.png")
+nextimg=PhotoImage(file="Graphics\\Nextb.png")
+previmg=PhotoImage(file="Graphics\\Prevb.png")
+stopimg=PhotoImage(file="Graphics\\Stopb.png")
+
+imagespace=Frame(masterFrame)
+imagespace.configure(bg="#F3872F")
+imagespace.grid(row = 1, column = 0, pady = 20)
+
+volumeFrame = LabelFrame(masterFrame, text = "Volume")
+volumeFrame.grid(row = 0, column = 1, padx = 20)
+volumeFrame.configure(bg="#F3872F")
 playbt=Button(imagespace,image=playimg,borderwidth=0,bg="#F3872F",command=playsong)
-pausebt=Button(imagespace,image=pauseimg,borderwidth=0,bg="#F3872F",command=pausesong)
+pausebt=Button(imagespace,image=pauseimg,borderwidth=0,bg="#F3872F",command=lambda: pausesong(pause_state))
 prevbt=Button(imagespace,image=previmg,borderwidth=0,bg="#F3872F",command=previousSong)
 nextbt=Button(imagespace,image=nextimg,borderwidth=0,bg="#F3872F",command=nextSong)
 stopbt=Button(imagespace,image=stopimg,borderwidth=0,bg="#F3872F",command=stopsong)
@@ -387,6 +485,7 @@ shufbt.grid(row=0,column=5)
 
 
 imagespace2=Frame(loop)
+imagespace2.configure(bg="#F3872F")
 imagespace2.pack()
 name = Label(imagespace2, text="Name:").grid(row=7, column = 1)
 
@@ -400,6 +499,7 @@ bt1.grid(row=8, column=4)
 bt2 = Button(imagespace2, text="Store Playlist", command=lambda :storeuser(name_entry.get()))
 bt2.grid(row=8, column=6)
 imagespace1=Frame(loop)
+imagespace1.configure(bg="#F3872F")
 imagespace1.pack()
 
 
@@ -434,10 +534,23 @@ dropremovemenu.config(bg="#ffffff")
 
 Themes=Menu(menu1)
 menu1.add_cascade(label= "Themes",menu=Themes)
-Themes.add_command(label="Monochrome",command = lambda: Themesetter(loop,playground,playbt,pausebt,nextbt,prevbt,stopbt,"MONOCHROME"))
+Themes.add_command(label="Monochrome",command=lambda: Themesetter(scrubFrame,shufbt,imagespace,imagespace1,imagespace2,loop,masterFrame,volumeFrame,playground,playbt,pausebt,nextbt,prevbt,stopbt,postime,lentime,"MONOCHROME"))
 Themes.config(bg="#ffffff")
-Themes.add_command(label="Glacier",command=lambda: Themesetter(loop,playground,playbt,pausebt,nextbt,prevbt,stopbt,"Glacier"))
-Themes.add_command(label="Autumn Leaves",command=lambda: Themesetter(loop,playground,playbt,pausebt,nextbt,prevbt,stopbt,"Autumn Leaves"))
-Themes.add_command(label="Spring",command=lambda: Themesetter(loop,playground,playbt,pausebt,nextbt,prevbt,stopbt,"Spring"))
-Themes.add_command(label="Summer",command=lambda: Themesetter(loop,playground,playbt,pausebt,nextbt,prevbt,stopbt,"Summer"))
+Themes.add_command(label="Glacier",command=lambda: Themesetter(scrubFrame,shufbt,imagespace,imagespace1,imagespace2,loop,masterFrame,volumeFrame,playground,playbt,pausebt,nextbt,prevbt,stopbt,postime,lentime,"Glacier"))
+Themes.add_command(label="Autumn Leaves",command=lambda: Themesetter(scrubFrame,shufbt,imagespace,imagespace1,imagespace2,loop,masterFrame,volumeFrame,playground,playbt,pausebt,nextbt,prevbt,stopbt,postime,lentime,"Autumn Leaves"))
+Themes.add_command(label="Spring",command=lambda: Themesetter(scrubFrame,shufbt,imagespace,imagespace1,imagespace2,loop,masterFrame,volumeFrame,playground,playbt,pausebt,nextbt,prevbt,stopbt,postime,lentime,"Spring"))
+Themes.add_command(label="Summer",command=lambda: Themesetter(scrubFrame,shufbt,imagespace,imagespace1,imagespace2,loop,masterFrame,volumeFrame,playground,playbt,pausebt,nextbt,prevbt,stopbt,postime,lentime,"Summer"))
+scrubFrame = LabelFrame(masterFrame)
+scrubFrame.configure(bg="#F3872F")
+scrubFrame.grid(row = 2, column = 0)
+sliderVolume = ttk.Scale(volumeFrame, from_ = 0, to = 1, orient = VERTICAL, value = 0, command = volume, length = 100)
+sliderVolume.pack(pady = 10)
+timescrub=ttk.Scale(scrubFrame,from_=0, to=100, orient=HORIZONTAL,value=0,command=scrub,length=300)
+timescrub.pack(pady=20)
+postime=Label(scrubFrame,text="0:00",bg="#F3872F")
+postime.pack(after=timescrub,pady=2)
+lentime=Label(scrubFrame,text="0:00",bg="#F3872F")
+lentime.pack(after=postime,pady=2)
+
 loop.mainloop()
+
